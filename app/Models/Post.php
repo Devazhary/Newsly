e0f499250a7fec705cf83bb6828cfdfc27be601d
Ahmed elazhary <ahmedelazhary804@gmail.com>
2026-02-15 20:57:41 +0200

    first commit

---
+++ app/Models/Post.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class Post extends Model
+{
+    use HasFactory;
+    protected $fillable = [
+        'title',
+        'description',
+        'slug',
+        'commentable',
+        'user_id',
+        'category_id',
+        'status',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    public function comments()
+    {
+        return $this->hasMany(Comment::class);
+    }
+
+    public function images()
+    {
+        return $this->hasMany(Image::class);
+    }
+}
