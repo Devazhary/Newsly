27775c16f55dddefc845e22dc0fd5ca7d89abf43
Ahmed elazhary <ahmedelazhary804@gmail.com>
2026-02-16 23:10:11 +0200

    Add authentication views and styles
    - Created SASS variables for body background and typography.
    - Added main SASS file to import variables and Bootstrap.
    - Implemented login, registration, password reset, and email confirmation views.
    - Developed layout for the application with navigation and content sections.
    - Included dashboard view for logged-in users.

--- routes/web.php
+++ routes/web.php
@@ -2,5 +2,10 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\frontend\HomeController;
+use Illuminate\Support\Facades\Auth;
+
+
+Auth::routes();
 
 Route::get('/', [HomeController::class, 'index'])->name('frontend.index');
+
